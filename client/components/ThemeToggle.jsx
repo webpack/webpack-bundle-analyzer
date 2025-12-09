fb031aee2b1f5df4553e533986a2898750f8fef2
Aditya <adityasuri11ss@gmail.com>
2025-12-09 10:17:55 +0530

    feat: add ThemeToggle component with moon/sun icons

---
+++ client/components/ThemeToggle.jsx
@@ -0,0 +1,28 @@
+import {Component} from 'preact';
+import {observer} from 'mobx-react';
+
+import s from './ThemeToggle.css';
+import Button from './Button';
+import Icon from './Icon';
+import {store} from '../store';
+
+@observer
+export default class ThemeToggle extends Component {
+  render() {
+    const {darkMode} = store;
+
+    return (
+      <Button
+        type="button"
+        title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        className={s.themeToggle}
+        onClick={this.handleToggle}>
+        <Icon name={darkMode ? 'sun' : 'moon'} size={16}/>
+      </Button>
+    );
+  }
+
+  handleToggle = () => {
+    store.toggleDarkMode();
+  }
+}
