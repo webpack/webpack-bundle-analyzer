5ea7d739525f7f6c648b5650a79609c8d084949d
alexander-akait <sheo13666q@gmail.com>
2026-01-22 13:16:26 +0300

    refactor: rebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
-const BundleAnalyzePlugin = require('./lib/BundleAnalyzerPlugin');
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzePlugin = require("./lib/BundleAnalyzerPlugin");
 
 const isDev = (process.env.NODE_ENV || "production") === "development";
 const needAnalyze = process.env.ANALYZE || false;
@@ -8,24 +8,24 @@ const needAnalyze = process.env.ANALYZE || false;
 module.exports = {
   mode: process.env.NODE_ENV || "production",
   context: __dirname,
-  entry: './client/viewer',
+  entry: "./client/viewer",
   output: {
     path: `${__dirname}/public`,
-    filename: 'viewer.js',
-    publicPath: '/'
+    filename: "viewer.js",
+    publicPath: "/",
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
     alias: {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
-      mobx: require.resolve('mobx/lib/mobx.es6.js')
-    }
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      mobx: require.resolve("mobx/lib/mobx.es6.js"),
+    },
   },
-  devtool: isDev ? 'eval' : 'source-map',
+  devtool: isDev ? "eval" : "source-map",
   performance: {
-    hints: false
+    hints: false,
   },
   optimization: {
     minimize: !isDev,
@@ -33,101 +33,110 @@ module.exports = {
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: /copyright/iu
+            comments: /copyright/iu,
           },
-          safari10: true
-        }
-      })
-    ]
+          safari10: true,
+        },
+      }),
+    ],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/u,
         exclude: /node_modules/u,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
           babelrc: false,
           presets: [
-            ['@babel/preset-env', {
-              // Target browsers are specified in .browserslistrc
+            [
+              "@babel/preset-env",
+              {
+                // Target browsers are specified in .browserslistrc
 
-              modules: false,
-              useBuiltIns: 'usage',
-              corejs: require('./package.json').devDependencies['core-js'],
-              debug: true
-            }],
-            ['@babel/preset-react', {
-              runtime: 'automatic',
-              importSource: 'preact'
-            }]
+                modules: false,
+                useBuiltIns: "usage",
+                corejs: require("./package.json").devDependencies["core-js"],
+                debug: true,
+              },
+            ],
+            [
+              "@babel/preset-react",
+              {
+                runtime: "automatic",
+                importSource: "preact",
+              },
+            ],
           ],
           plugins: [
-            'lodash',
-            ['@babel/plugin-proposal-decorators', {legacy: true}],
-            ['@babel/plugin-transform-class-properties', {loose: true}],
-            ['@babel/plugin-transform-runtime', {
-              useESModules: true
-            }]
-          ]
-        }
+            "lodash",
+            ["@babel/plugin-proposal-decorators", { legacy: true }],
+            ["@babel/plugin-transform-class-properties", { loose: true }],
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true,
+              },
+            ],
+          ],
+        },
       },
       {
         test: /\.css$/u,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: {
-                localIdentName: '[name]__[local]'
+                localIdentName: "[name]__[local]",
               },
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
                 plugins: [
-                  require('postcss-icss-values'),
-                  require('autoprefixer'),
-                  ...(!isDev ? [require('cssnano')()] : [])
-                ]
-              }
-            }
-          }
-        ]
+                  require("postcss-icss-values"),
+                  require("autoprefixer"),
+                  ...(!isDev ? [require("cssnano")()] : []),
+                ],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/u,
-        loader: 'url-loader'
-      }
-    ]
+        loader: "url-loader",
+      },
+    ],
   },
-  plugins: (plugins => {
+  plugins: ((plugins) => {
     if (!isDev) {
       if (needAnalyze) {
         plugins.push(
           new BundleAnalyzePlugin({
-            generateStatsFile: true
-          })
+            generateStatsFile: true,
+          }),
         );
       }
 
       plugins.push(
         new webpack.DefinePlugin({
-          'process': JSON.stringify({
+          process: JSON.stringify({
             env: {
-              NODE_ENV: 'production'
-            }
+              NODE_ENV: "production",
+            },
           }),
           // Fixes "ModuleConcatenation bailout" for some modules (e.g. Preact and MobX)
-          'global': 'undefined'
-        })
+          global: "undefined",
+        }),
       );
     }
 
     return plugins;
-  })([])
+  })([]),
 };
