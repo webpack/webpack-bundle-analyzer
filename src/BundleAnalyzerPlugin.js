6c634bab7d04fc7bee34f908e842fac1a2e797b7
Your Name <srushtikondhalkar7@gmail.com>
2026-02-11 10:18:03 -0500

    fix: prevent WebpackLogger 'done hook' error when callback throws

--- src/BundleAnalyzerPlugin.js
+++ src/BundleAnalyzerPlugin.js
@@ -69,10 +69,9 @@ class BundleAnalyzerPlugin {
         // Making analyzer logs to be after all webpack logs in the console
         setImmediate(async () => {
           try {
-            await Promise.all(actions.map((action) => action()));
+            await Promise.all(actions.map(action => action()));
+          } finally {
             callback();
-          } catch (err) {
-            callback(err);
           }
         });
       } else {
